fb5c2e01f89ae4a481627454b3d896833007c9a1
Julian Neuberger <neuberger.julian@gmail.com>
2018-07-10 15:19:11 +0200

    updated crawler to support multiple galleries

--- data/crawler.py
+++ data/crawler.py
@@ -8,11 +8,12 @@ import time
 
 
 class ImageDao:
-    def __init__(self, url, data, name, extension):
+    def __init__(self, url, data, name, extension, album):
         self.url = url
         self.data = data
         self.name = name
         self.extension = extension
+        self.album = album
 
 
 class Crawler:
@@ -21,8 +22,8 @@ class Crawler:
 
     @staticmethod
     def persist(image: ImageDao):
-        os.makedirs('img', exist_ok=True)
-        with open(os.path.join('img', image.name + image.extension), mode='wb') as file:
+        os.makedirs(os.path.join('img', image.album), exist_ok=True)
+        with open(os.path.join('img', image.album, image.name + image.extension), mode='wb') as file:
             file.write(image.data)
 
     def run(self):
@@ -36,37 +37,39 @@ class ImgurCrawler(Crawler):
     image_base = 'https://i.imgur.com/{}{}'
 
     def crawl(self) -> List[ImageDao]:
-        gallery_id = 'EFqYnTc'
-        print('Loading images from gallery {}'.format(gallery_id))
-        gallery_url = ImgurCrawler.gallery_base.format(gallery_id)
-        gallery_info = requests.request('get', gallery_url, params={'all': True})
-        print('Request finished with status {}'.format(gallery_info.status_code))
-        if gallery_info.status_code == requests.codes.OK:
-            gallery_info = json.loads(gallery_info.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-            gallery_info = gallery_info.data
-            print('Request ok, fetching {} images...'.format(gallery_info.count))
-            seconds_per_image = 0
-            ret = []
-            for index, image in enumerate(gallery_info.images):
-                start = time.time()
-                ret.append(self.crawl_single_image(image))
-                time_took = time.time() - start
-                seconds_per_image = (seconds_per_image * index + time_took) / (index + 1)
-                eta = seconds_per_image * (gallery_info.count - index)
-                print('Fetched image {} in {:.3f} seconds, eta {:.1f} seconds'.format(
-                    image.hash,
-                    time_took,
-                    eta
-                ))
-            return ret
+        ret = []
+        gallery_ids = 'EFqYnTc', 'qLSuLZD', '4g2jIUE', 'TYBDakN', 'EuTp2eO'
+        for gallery_id in gallery_ids:
+            print('Loading images from gallery {}'.format(gallery_id))
+            gallery_url = ImgurCrawler.gallery_base.format(gallery_id)
+            gallery_info = requests.request('get', gallery_url, params={'all': True})
+            print('Request finished with status {}'.format(gallery_info.status_code))
+            if gallery_info.status_code == requests.codes.OK:
+                gallery_info = json.loads(gallery_info.text, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+                gallery_info = gallery_info.data
+                print('Request ok, fetching {} images...'.format(gallery_info.count))
+                seconds_per_image = 0
+                for index, image in enumerate(gallery_info.images):
+                    start = time.time()
+                    ret.append(self.crawl_single_image(image, gallery_id))
+                    time_took = time.time() - start
+                    seconds_per_image = (seconds_per_image * index + time_took) / (index + 1)
+                    eta = seconds_per_image * (gallery_info.count - index)
+                    print('Fetched image {} in {:.3f} seconds, eta {:.1f} seconds'.format(
+                        image.hash,
+                        time_took,
+                        eta
+                    ))
+        return ret
 
     @staticmethod
-    def crawl_single_image(image):
+    def crawl_single_image(image, album):
         return ImageDao(
             url=ImgurCrawler.image_base.format(image.hash, image.ext),
             name=image.hash,
             extension=image.ext,
-            data=requests.request('get', ImgurCrawler.image_base.format(image.hash, image.ext)).content
+            data=requests.request('get', ImgurCrawler.image_base.format(image.hash, image.ext)).content,
+            album=album
         )
 
 
